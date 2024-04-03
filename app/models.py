5809c98bf4667d1b453b614c053dabf4e220a4ad
P33J3 <peterjonlawrence@gmail.com>
2024-04-03 14:30:44 -0500

    Completed Exercise 2

--- app/models.py
+++ app/models.py
@@ -1,1 +1,18 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+
+class Movie(db.Model):
+
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.String(256))
+    poster = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
